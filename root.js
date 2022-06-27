d6741bc45bc3114f7dd87fa5c0f00efd9909fb25
IRO119 <ivaniro911@live.com>
2022-06-27 13:49:54 +0900

    Second Commit

--- root.js
+++ root.js
@@ -3,3 +3,26 @@ const Discord = require("discord.js");
 require("dotenv").config();
 
 welcomeIdChannel = "990774765641936916/990774765641936922"
+
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+});
+
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+});
+
+
+client.on("messageCreate", (Message) => {
+    if(Message.content == "Howdy"){
+        Message.reply(`Howdy ${Message.author}`)
+    }
+})
+
+client.login(process.env.TOKEN)
